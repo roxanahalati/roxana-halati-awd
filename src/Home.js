2f7afe938d610187d04eed61144b9b62a5bb1bf2
rox-emotion <113882292+rox-emotion@users.noreply.github.com>
2024-01-14 10:13:26 +0200

    text change

--- src/Home.js
+++ src/Home.js
@@ -76,6 +76,7 @@ const Home = () => {
               Hi there! Thank you for visiting my website. Lorem Ipsum Dolor Sit Amet, consectetur adipiscing elit.
               Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
               exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              ldsldas
             </p>
           </div>
         </TextContainer>
