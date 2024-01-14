48bd0e0bb7f1349434a8b7e2f788640eafa8c6d8
rox-emotion <113882292+rox-emotion@users.noreply.github.com>
2024-01-14 10:16:25 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home';
-import PersonalInfo from './PersonalInfo';
 import Teaching from './Teaching';
 import Contact from './Contact';
 import Menu from './components/Menu';
